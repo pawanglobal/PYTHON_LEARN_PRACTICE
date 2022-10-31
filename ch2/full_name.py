4bbb9f4585898a9bcc8219ffe447a5084be0ca67
pawanglobal <pawanglobal07@gmail.com>
2022-10-31 22:41:38 +0100

    first commit

---
+++ ch2/full_name.py
@@ -0,0 +1,10 @@
+first_name = "pawan"
+last_name = "kumar"
+
+full_name = f"{first_name.title()} {last_name.title()}"
+print(full_name)
+
+print(f"Hello! {full_name.upper()}")
+
+message = f"Hello! {full_name} where are you?"
+print(message)
