94d94adf86f0df18a2aaaf1682412abc12c22bce
pawanglobal <pawanglobal07@gmail.com>
2022-12-27 01:11:13 +0100

    first commit

---
+++ while_loops.py
@@ -0,0 +1,10 @@
+elements =[]
+i = 0
+while i in range(0, 6):
+    print(f"The number I am going to add to elements is {i}.\n")
+    elements.append(i)
+    i = i + 1
+    print(f"After adding the i to the elements, i has increased by 1 now the i is {i}.")
+
+
+print(elements)
